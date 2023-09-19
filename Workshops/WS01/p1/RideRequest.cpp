64d0c9c7c30f86df9f15e828462252eee9100eea
leomoca19 <leomoca19@gmail.com>
2023-09-18 20:34:08 -0400

    added a break line afrer the citation comment

--- Workshops/WS01/p1/RideRequest.cpp
+++ Workshops/WS01/p1/RideRequest.cpp
@@ -6,7 +6,8 @@ Date of completion: 15-September-2023
 
 I have done all the coding by myself and only copied the code
 that my professor provided to complete my workshops and assignments.
-***********************************************************************/#include "RideRequest.h"
+***********************************************************************/
+#include "RideRequest.h"
 #include <iostream>
 using namespace std;
 
