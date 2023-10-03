2c4415596062e32ec83301b69c379a2cdb035f7b
Leonardo_Caceres <leomoca19@gmail.com>
2023-10-03 18:56:31 -0400

    Cheese(str) started

---
+++ Workshops/WS04/p1/Cheese.cpp
@@ -0,0 +1,19 @@
+/***********************************************************************
+Author: Leonardo de la Mora Caceres
+Student number: 152877205
+Email: lde-la-mora-canceres@myseneca.ca
+Date of completion: September-6-2023
+
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+***********************************************************************/
+#include "Cheese.h"
+using namespace std;
+namespace sdds {
+	Cheese::Cheese(const string& str)
+	{
+		//tokenize str
+		//Name,Weight,Price,Features
+
+	}
+}
