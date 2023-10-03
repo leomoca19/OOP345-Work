ba9f8634dd900cba4a3fad08a02eb20ae53069a6
Leonardo_Caceres <leomoca19@gmail.com>
2023-10-03 18:34:21 -0400

    Cheese class and properties created

---
+++ Workshops/WS04/p1/Cheese.h
@@ -0,0 +1,23 @@
+/***********************************************************************
+Author: Leonardo de la Mora Caceres
+Student number: 152877205
+Email: lde-la-mora-canceres@myseneca.ca
+Date of completion: September-6-2023
+
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+***********************************************************************/
+#ifndef SDDS_CHEESE_H
+#define SDDS_CHEESE_H
+#include <iostream>
+#include <string>
+namespace sdds {
+	class Cheese {
+		std::string name{"NaC"};
+		size_t weight{};
+		double price{};
+		std::string features{};
+
+	};
+}
+#endif
