1b803efc08b1b5f748b687679dd05a0602e2527b
Leonardo_Caceres <leomoca19@gmail.com>
2023-10-03 18:46:44 -0400

    Cheese methods declared

--- Workshops/WS04/p1/Cheese.h
+++ Workshops/WS04/p1/Cheese.h
@@ -17,6 +17,36 @@ namespace sdds {
 		size_t weight{};
 		double price{};
 		std::string features{};
+
+	public:
+		//- a default constructor
+		//- `Cheese(const std::string& str)`: A constructor that receives the cheese's details as a string; this constructor is responsible for extracting information about the cheese from the string and storing the tokens in the instance's attributes. The string will always have the following format:
+		//  ```
+		//  Name,Weight,Price,Features
+		//  ```
+		//  Every token that follows the price is a feature of the cheese (e.g., for `"Cheddar, 1200, 2.99, Hard, Sharp, Smooth"` the features are *Hard*, *Sharp*, and *Smooth*). Each of extracted tokens should be stored in the respective data member; the features should be stored as a space separated list of strings (e.g., `"Hard Sharp Smooth"`).
+		//  This constructor should remove all leading and trailing spaces from the **beginning and end** of any token extracted from parameter.
+		//  When implementing the constructor, consider these following functions:
+		//  - [std::string::substr()](https://en.cppreference.com/w/cpp/string/basic_string/substr)
+		//  - [std::string::find()](https://en.cppreference.com/w/cpp/string/basic_string/find)
+		//  - [std::string::find_first_of()](https://en.cppreference.com/w/cpp/string/basic_string/find_first_of)
+		//  - [std::string::erase()](https://en.cppreference.com/w/cpp/string/basic_string/erase)
+		//  - [std::stoul()](https://en.cppreference.com/w/cpp/string/basic_string/stoul)
+		//  - [std::stod()](https://en.cppreference.com/w/cpp/string/basic_string/stod)
+		Cheese(const std::string& str = "");
+
+
+		//- `Cheese slice(size_t weight)`: a modifier that receives as a parameter a weight/amount of cheese to slice from the current cheese object.
+		//  If there is enough cheese to make this slice (weight <= weight of the cheese in current object), then return a copy of current cheese with the desired weight. Update the current object's weight after slicing.
+		//  If there isn't enough cheese, then return a cheese object that is in an empty state instead.
+		Cheese slice(size_t weight);
+
+
+		//- A set of queries to retrieve the value of each attribute of the cheese.
+		std::string& getName() const;
+		size_t& getWeight() const;
+		double price() const;
+		std::string& getFeatures() const;
 	};
 }
 #endif
