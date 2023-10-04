229a830b0221e56f1699ce6df298dc2b216d8d67
leomoca19 <leomoca19@gmail.com>
2023-10-03 23:02:36 -0400

    read & print operators and methods started

--- Workshops/WS04/p1/Cheese.cpp
+++ Workshops/WS04/p1/Cheese.cpp
@@ -12,8 +12,52 @@ using namespace std;
 namespace sdds {
 	Cheese::Cheese(const string& str)
 	{
-		//tokenize str
 		//Name,Weight,Price,Features
+		size_t strFirst = str.find_first_not_of(" \t\n\r"),
+			strLast = str.find_first_of(',') - 1;
+		name = str.substr(strFirst, strLast);
 
+		strFirst = str.find_first_not_of(" \t\n\r");
+		strLast = str.find_first_of(',') - 1;
+	}
+
+	Cheese Cheese::slice(size_t weight)
+	{
+		return *this;
+	}
+
+	std::string Cheese::getName() const
+	{
+		return name;
+	}
+	size_t Cheese::getWeight() const
+	{
+		return weight;
+	}
+	double Cheese::getPrice() const
+	{
+		return price;
+	}
+	std::string Cheese::getFeatures() const
+	{
+		return features;
+	}
+
+	std::ostream& Cheese::print(std::ostream& os) const
+	{
+		return os;
+	}
+	std::istream& Cheese::read(std::istream& is)
+	{
+		return is;
+	}
+
+	std::ostream& operator<<(std::ostream& os, const Cheese& right)
+	{
+		return right.print(os);
+	}
+	std::istream& operator>>(std::istream& is, Cheese& right)
+	{
+		return right.read(is);
 	}
 }
