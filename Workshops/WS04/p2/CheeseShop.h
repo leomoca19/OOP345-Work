5fd0084e46612fd5004b78abbafa9df88bb4f67d
leomoca19 <leomoca19@gmail.com>
2023-10-06 10:49:03 -0400

    Declared constructors, assgn, insrtn and index oprtrs, and addCheese()

--- Workshops/WS04/p2/CheeseShop.h
+++ Workshops/WS04/p2/CheeseShop.h
@@ -9,11 +9,22 @@ that my professor provided to complete my workshops and assignments.
 ***********************************************************************/
 #ifndef SDDS_CHEESESHOP_H
 #define SDDS_CHEESESHOP_H
+#include <iostream>
 #include "Cheese.h"
 namespace sdds {
 	class CheeseShop {
 		const Cheese** cheeses;
+		std::string name;
+		size_t size;
+	public:
+		CheeseShop(const std::string& name = "No name");
+		CheeseShop(const CheeseShop& other);
+		CheeseShop& operator=(const CheeseShop& other);
 
+		CheeseShop& addCheese(const Cheese& other);
+
+		CheeseShop& operator[](const size_t& i) const;
+		Cheese& operator>>(const Cheese& other);
 	};
 }
 #endif //!SDDS_CHEESESHOP_H
