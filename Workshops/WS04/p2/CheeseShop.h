53e43959310fe77397dbe27c1df42da04a5fff2a
leomoca19 <leomoca19@gmail.com>
2023-10-06 10:19:40 -0400

    file created, signature and safeguards added

---
+++ Workshops/WS04/p2/CheeseShop.h
@@ -0,0 +1,15 @@
+/***********************************************************************
+Author: Leonardo de la Mora Caceres
+Student number: 152877205
+Email: lde-la-mora-canceres@myseneca.ca
+Date of completion: October-6-2023
+
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+***********************************************************************/
+#ifndef SDDS_CHEESE_H
+#define SDDS_CHEESE_H
+namespace sdds {
+
+}
+#endif //!SDDS_CHEESE_H
