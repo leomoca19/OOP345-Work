535978bc8d8eb8b2b81ddb2b366bfb7f2bb0b694
leomoca19 <leomoca19@gmail.com>
2023-10-06 10:17:46 -0400

    Completion date fixed

--- Workshops/WS04/p2/Cheese.cpp
+++ Workshops/WS04/p2/Cheese.cpp
@@ -2,7 +2,7 @@
 Author: Leonardo de la Mora Caceres
 Student number: 152877205
 Email: lde-la-mora-canceres@myseneca.ca
-Date of completion: September-6-2023
+Date of completion: October-6-2023
 
 I have done all the coding by myself and only copied the code
 that my professor provided to complete my workshops and assignments.
