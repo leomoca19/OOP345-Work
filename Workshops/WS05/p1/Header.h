46a4907ee71f8f772fed3ffe75a484ad66c62c44
leomoca19 <leomoca19@gmail.com>
2023-10-18 22:30:52 -0400

    renamed Header.h to Book.h and attributes defined

---
+++ Workshops/WS05/p1/Header.h
@@ -0,0 +1,18 @@
+/***********************************************************************
+Author: Leonardo de la Mora Caceres
+Student number: 152877205
+Email: lde-la-mora-canceres@myseneca.ca
+Date of completion: October-20-2023
+
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+***********************************************************************/
+#ifndef SDDS_BOOK_H
+#define SDDS_BOOK_H
+#include <iostream>
+namespace sdds {
+	class Book {
+
+	};
+}
+#endif // !SDDS_BOOK_H
