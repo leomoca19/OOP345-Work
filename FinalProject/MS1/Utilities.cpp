1fdff1b81f1a81cc2185324757f20cc0b9bec712
leomoca19 <leomoca19@gmail.com>
2023-11-14 23:59:45 -0500

    extract Token implementation started

--- FinalProject/MS1/Utilities.cpp
+++ FinalProject/MS1/Utilities.cpp
@@ -16,7 +16,29 @@ namespace sdds {
 		return m_widthField;
 	}
 
-	string Utilities::extractToken(const string& str, size_t& next_pos, bool& more){}
+	string Utilities::extractToken(const string& str, size_t& next_pos, bool& more){
+		string str_{};
+		size_t i{ next_pos };
+		bool success{};
+
+		//uses the delimiter to extract the next token from `str` starting at position `next_pos`
+		while (str[i] != m_delimiter) {
+
+		}
+
+
+		if (success) {
+			///	-If successful, return a copy of the extracted token found(without spaces at the 
+			///	beginning / end),    update `next_pos` with the position of the next token, and set 
+			///	`more` to `true` (`false` otherwise)
+		}
+		///		-reports an exception if a delimiter is found at `next_pos`
+		///		-updates the current object's `m_widthField` data member if its current value is less 
+		///		than the size	of the token extracted
+
+
+		return str_;
+	}
 
 	void Utilities::setDelimiter(char newDelimiter){}
 
