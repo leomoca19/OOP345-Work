2e3d916cf2f29e1509f4e36719334d3490035297
leomoca19 <leomoca19@gmail.com>
2023-11-14 19:30:10 -0500

    initial setup

---
+++ FinalProject/MS1/Utilities.h
@@ -0,0 +1,13 @@
+// Name: Leonardo de la mora Caceres
+// Seneca Student ID: 152877205
+// Seneca email: lde-la-mora-canceres@myseneca.ca
+// Date of completion: 2023-Nov-14
+//
+// I confirm that I am the only author of this file
+// and the content was created entirely by me.
+#ifndef UTILITIES_H
+#define UTILITIES_H
+namespace sdds {
+
+}
+#endif // !UTILITIES_H
