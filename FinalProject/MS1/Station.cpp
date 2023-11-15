0d5058f476304e220ffac05cb2b36b620797ae2e
leomoca19 <leomoca19@gmail.com>
2023-11-15 00:08:53 -0500

    empty declarations

---
+++ FinalProject/MS1/Station.cpp
@@ -0,0 +1,35 @@
+// Name: Leonardo de la mora Caceres
+// Seneca Student ID: 152877205
+// Seneca email: lde-la-mora-canceres@myseneca.ca
+// Date of completion: 2023-Nov-14
+//
+// I confirm that I am the only author of this file
+// and the content was created entirely by me.
+#include "Station.h"
+using namespace std;
+namespace sdds{
+	Station::Station(const std::string& record) {}
+
+	const string& Station::getItemName() const {
+		return string();
+	}
+
+	size_t Station::getNextSerialNumber()
+	{
+		return size_t();
+	}
+
+	size_t Station::getQuantity() const
+	{
+		return size_t();
+	}
+
+	void Station::updateQuantity()
+	{
+	}
+
+	void Station::display(std::ostream& os, bool full) const
+	{
+	}
+
+}
