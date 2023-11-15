15decb87e2f28ff06ed07314c0f564228aec25e6
leomoca19 <leomoca19@gmail.com>
2023-11-14 22:07:55 -0500

    initial setup

---
+++ FinalProject/MS1/Utilities.cpp
@@ -0,0 +1,22 @@
+// Name: Leonardo de la mora Caceres
+// Seneca Student ID: 152877205
+// Seneca email: lde-la-mora-canceres@myseneca.ca
+// Date of completion: 2023-Nov-14
+//
+// I confirm that I am the only author of this file
+// and the content was created entirely by me.
+#include "Utilities.h"
+using namespace std;
+namespace sdds {
+	void Utilities::setFieldWidth(size_t newWidth) {
+
+	}
+
+	size_t Utilities::getFieldWidth() const{}
+
+	string Utilities::extractToken(const string& str, size_t& next_pos, bool& more){}
+
+	void Utilities::setDelimiter(char newDelimiter){}
+
+	char Utilities::getDelimiter() {}
+}
