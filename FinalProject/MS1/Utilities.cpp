7e92c18a7d3e9e941c6d3e65b65ef11f408d82d4
leomoca19 <leomoca19@gmail.com>
2023-11-16 04:20:14 -0500

    extractToken finished

--- FinalProject/MS1/Utilities.cpp
+++ FinalProject/MS1/Utilities.cpp
@@ -5,29 +5,47 @@
 //
 // I confirm that I am the only author of this file
 // and the content was created entirely by me.
+#include <algorithm>
 #include "Utilities.h"
 using namespace std;
 namespace sdds {
 	char Utilities::m_delimiter = '\0';
 
-	void Utilities::setFieldWidth(size_t newWidth){
+	void Utilities::setFieldWidth(size_t newWidth) {
 		m_widthField = newWidth;
 	}
 
-	size_t Utilities::getFieldWidth() const{
+	size_t Utilities::getFieldWidth() const {
 		return m_widthField;
 	}
 
-	string Utilities::extractToken(const string& str, size_t& next_pos, bool& more){
-        size_t delimiter_pos = str.find(m_delimiter, next_pos);
-        std::string token;
+	string Utilities::extractToken(const string& str, size_t& next_pos, bool& more) {
+		size_t delimiter_pos = str.find(m_delimiter, next_pos);
+		std::string token{};
+		size_t trimStart{},
+			trimEnd{};
 
-		//To be completed
+		if (delimiter_pos != next_pos)
+		{
+			token = str.substr(next_pos, delimiter_pos - next_pos);
+			next_pos = delimiter_pos + 1;
+		}
+		else
+		{
+			throw "delimiter found at next_pos";
+			more = 0;
+		}
+
+		trimStart = token.find_first_not_of(' ');
+		trimEnd = token.find_last_not_of(' ');
+		token = token.substr(trimStart, trimStart - trimEnd);
+
+		m_widthField = max(m_widthField, token.length());
 
 		return token;
 	}
 
-	void Utilities::setDelimiter(char newDelimiter){
+	void Utilities::setDelimiter(char newDelimiter) {
 		m_delimiter = newDelimiter;
 	}
 
