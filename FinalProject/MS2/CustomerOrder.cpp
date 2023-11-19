48dee40ea0cc1c8d581b310f48320b85ac6b276e
leomoca19 <leomoca19@gmail.com>
2023-11-19 00:28:54 -0500

    empty definitions and 1 arg constructor started

--- FinalProject/MS2/CustomerOrder.cpp
+++ FinalProject/MS2/CustomerOrder.cpp
@@ -5,8 +5,50 @@
 //
 // I confirm that I am the only author of this file
 // and the content was created entirely by me.
+#include "Utilities.h"
 #include "CustomerOrder.h"
 using namespace std;
 namespace sdds {
+	size_t CustomerOrder::m_widthField{};
+	CustomerOrder::CustomerOrder(const std::string& record) : m_cntItem{}
+	{
+		Utilities util{};
+		size_t next_pos{};
+		bool more{ 1 };
 
+		m_name = util.extractToken(record, next_pos, more);
+		m_product = util.extractToken(record, next_pos, more);
+
+		m_lstItem = new Item * [m_cntItem];
+
+		while (more) {
+			string itemname = util.extractToken(record, next_pos, more);
+			m_lstItem[m_cntItem++] = new Item(itemname);
+			m_widthField = max(util.getFieldWidth(), m_widthField);
+		}
+	}
+	CustomerOrder::CustomerOrder(CustomerOrder&& other) noexcept
+	{
+	}
+	CustomerOrder& CustomerOrder::operator=(CustomerOrder&& other) noexcept
+	{
+		return *this;
+	}
+	CustomerOrder::~CustomerOrder()
+	{
+	}
+	bool CustomerOrder::isOrderFilled() const
+	{
+		return false;
+	}
+	bool CustomerOrder::isItemFilled(const std::string& itemName) const
+	{
+		return false;
+	}
+	void CustomerOrder::fillItem(Station& station, std::ostream& os)
+	{
+	}
+	void CustomerOrder::display(std::ostream& os) const
+	{
+	}
 }
