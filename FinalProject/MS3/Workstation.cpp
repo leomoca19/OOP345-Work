376d6123794a63a78c151eb2e0e70dd0f43e9d6e
leomoca19 <leomoca19@gmail.com>
2023-11-29 05:50:07 -0500

    initial setup

---
+++ FinalProject/MS3/Workstation.cpp
@@ -0,0 +1,12 @@
+// Name: Leonardo de la mora Caceres
+// Seneca Student ID: 152877205
+// Seneca email: lde-la-mora-canceres@myseneca.ca
+// Date of completion: 2023-Nov-29
+//
+// I confirm that I am the only author of this file
+// and the content was created entirely by me.
+#include "Workstation.h"
+using namespace std;
+namespace sdds {
+
+}
