15ce3ce837bc16c6a9e3576cb86ee6c7e73acb47
leomoca19 <leomoca19@gmail.com>
2023-11-29 04:17:32 -0500

    initial setup

---
+++ FinalProject/MS3/LineManager.h
@@ -0,0 +1,13 @@
+// Name: Leonardo de la mora Caceres
+// Seneca Student ID: 152877205
+// Seneca email: lde-la-mora-canceres@myseneca.ca
+// Date of completion: 2023-Nov-29
+//
+// I confirm that I am the only author of this file
+// and the content was created entirely by me.
+#ifndef SDDS_LINEMANAGER_H
+#define SDDS_LINEMANAGER_H
+namespace sdds {
+
+}
+#endif // !SDDS_LINEMANAGER_H
