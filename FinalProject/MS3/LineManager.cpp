504adc5c915b082a6c802fa1fe1189545bc0bffc
leomoca19 <leomoca19@gmail.com>
2023-12-05 07:31:35 -0500

    constructor, reorderStations, run and dsplay implemented

--- FinalProject/MS3/LineManager.cpp
+++ FinalProject/MS3/LineManager.cpp
@@ -5,79 +5,90 @@
 //
 // I confirm that I am the only author of this file
 // and the content was created entirely by me.
-#include <string>
+#include <algorithm>
+#include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
 #include "LineManager.h"
 #include "Utilities.h"
 using namespace std;
 namespace sdds {
-	LineManager::LineManager(const string& file, const vector<Workstation*>& stations)
-	{
+	LineManager::LineManager(const std::string& filename, const std::vector<Workstation*>& stations) {
 		Utilities util;
-		string record{};
-		ifstream input(file);
-
-		if (!input)
-			throw string("Unable to open file ") + file;
-
-
-		while (getline(input, record)) {
-			size_t next_pos{ 0 };
-			bool more{ true };
-			string current{}, next{};
-
-			// Extract current and next workstation names from the record
-			current = util.extractToken(record, next_pos, more);
-			next = util.extractToken(record, next_pos, more);
-
-			// Define lambda functions to find workstations by name
-			auto findCurrent = [&](const Workstation* station) { 
-				return station->getItemName() == current; 
-			};
-			auto findNext = [&](const Workstation* station) { 
-				return station->getItemName() == next; 
-			};
-
-			// Find iterators pointing to the current and next workstations in the collection
-			auto currentIt = find_if(stations.begin(), stations.end(), findCurrent);
-			auto nextIt = find_if(stations.begin(), stations.end(), findNext);
-
-			// Check if both current and next workstations are found in the collection
-			if (currentIt != stations.end() && nextIt != stations.end())
-				// Link the current workstation to the next workstation
-				(*currentIt)->setNextStation(*nextIt);
-			else
-				// Throw an exception if the linkage is unsuccessful
-				throw string("Unable to link stations [") + current + "] to [" + next + "].";
+		ifstream file(filename);
+		if (!file) {
+			throw "Incorrect file name!";
 		}
-
-		// Reserve space for the m_activeLine vector to improve performance
-		m_activeLine.reserve(stations.size());
-
-		// Find the iterator pointing to the first station on the assembly line
-		auto it = find_if(stations.begin(), stations.end(),
-			[&](const Workstation* station) { 
-				return station->getNextStation() == nullptr; 
+		else {
+			string str = "";
+			string currentStr = "";
+			string nextStr = "";
+			bool more;
+			size_t next_pos = 0;
+			Workstation* currentStation = nullptr;
+			Workstation* nextStation = nullptr;
+			Workstation* firstStation = nullptr;
+			while (getline(file, str)) {
+				currentStr = util.extractToken(str, next_pos, more);
+				currentStation = *find_if(stations.begin(), stations.end(), [&](Workstation* station) {
+					return station->getItemName() == currentStr;
+					});
+				m_activeLine.push_back(currentStation);
+				if (more) {
+					nextStr = util.extractToken(str, next_pos, more);
+					nextStation = *find_if(stations.begin(), stations.end(), [&](Workstation* station) {
+						return station->getItemName() == nextStr;
+						});
+					currentStation->setNextStation(nextStation);
+				}
 			}
-		);
-
-		// Check if the first station is found
-		if (it != stations.end()) {
-			m_firstStation = *it;
-			m_cntCustomerOrder = g_pending.size();
+			for_each(stations.begin(), stations.end(), [&](Workstation* station) {
+				firstStation = *find_if(stations.begin(), stations.end(), [&](Workstation* station) {
+					return station->getNextStation() == firstStation;
+					});
+				});
+			m_firstStation = firstStation;
 		}
-		else 
-			throw string("Unable to find the first station on the assembly line.");
+		file.close();
+		m_cntCustomerOrder = g_pending.size();
 	}
+
 	void LineManager::reorderStations()
 	{
+		Workstation* station = m_firstStation;
+		vector<Workstation*> reordered;
+
+		while (station) {
+			reordered.push_back(station);
+			station = station->getNextStation();
+		}
+
+		m_activeLine = reordered;
 	}
 	bool LineManager::run(ostream& os)
 	{
-		bool filled{};
-		return filled;
+		static size_t count = 1;
+
+		os << "Line Manager Iteration: " << count++ << '\n';
+
+		if (!g_pending.empty()) {
+			*m_firstStation += move(g_pending.front());
+			g_pending.pop_front();
+		}
+
+		for (const auto& station : m_activeLine)
+			station->fill(os);
+
+		for (const auto& station : m_activeLine)
+			station->attemptToMoveOrder();
+
+		return m_cntCustomerOrder == g_completed.size() + g_incomplete.size();
+	
 	}
 	void LineManager::display(ostream& os) const
 	{
+		for (const auto& station : m_activeLine)
+			station->display(os);
 	}
 }
